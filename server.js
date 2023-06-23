c930769e3aab872b68269c04f2da283d3da7185b
Xinan <youkingan11@gmail.com>
2023-06-23 14:46:12 +0100

    add libraries

--- server.js
+++ server.js
@@ -1,21 +1,47 @@
 // Import libraries that we installed using npm
+// In node.js, we hashing password using a library named bcrypt
+// In node.js, passport is a simple library to authenticate users
 
-const express = require("express")
-const bcrypt = require("bcrypt")
-const app = express()
+const express = require("express");
+const bcrypt = require("bcrypt");
+const app = express();
+
+// Here we simply store all users in an array, but a robust app needs a database
+
+const users = [];
+
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    })
+    res.redirect("/login")
+  } 
+  catch {
+    console.log(e);
+    alert("Registered Successfully, Redirecting to Login Page...");
+    res.redirect("/register")
+  }
+});
 
 // Routes
-app.get('/',(req,res)=>{
-    res.render("index.ejs")
-})
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-app.get('/login',(req,res)=>{
-    res.render("login.ejs")
-})
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
 
-app.get('/register',(req,res)=>{
-    res.render("register.ejs")
-})
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
 
 // End outes
-app.listen(3000)
+app.listen(3000);
