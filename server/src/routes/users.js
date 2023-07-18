7e0aae50a7d87a31455f1f5c4213469b185ffe4c
Xinan <youkingan11@gmail.com>
2023-07-18 12:18:17 +0100

    finished register and login routes

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -16,11 +16,31 @@ router.post("/register", async (req, res) => {
     // To get the username and password from the request body
     const { username, password } = req.body;
     // To check if the username already exists in the database
+    // username = username: username
     const user = await UserModel.findOne({username});
-    // If the username already exists, send back a response to the frontend
-    res.json(user);
+    // If the username already exists...
+    if (user){
+        return res.json({error: "Username already exists."});
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new UserModel({username, password: hashedPassword});
+    await newUser.save();
+    res.json({message: "User registered successfully!"});
 });
 
-router.post("/login")
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    const user = await UserModel.findOne({username});
+    if (!user){
+        return res.json({message: "User doesn't exist."});
+    };
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid){
+        return res.json({message: "Password is incorrect!"});
+    };
+    // if the username and password are valid, we will create a token
+    const token = jwt.sign({id: user._id},"secret");
+    res.json({token, userID: user._id});
+});
 
 export { router as userRouter };
